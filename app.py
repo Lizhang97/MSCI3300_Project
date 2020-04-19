1051ed142e5473b2a4700df5f9539c3f8b276959
Xinwen Zhang <xinwen-zhang@uiowa.edu>
2020-04-18 22:43:41 -0500

    Environment Variables

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from functools import wraps
-
 import pymysql
 import secrets
+import os
+
+dbuser=os.environ.get('DBUSER')
+dbpass=os.environ.get('DBPASS')
+dbhost=os.environ.get('DBHOST')
+dbname=os.environ.get('DBNAME')
 
 
-conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
-
+#conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+conn="mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser,dbpass,dbhost,dbname)
 # Open database connection
 #dbhost = secrets.dbhost
 #dbuser = secrets.dbuser
